bd327ca2373c4e800e89d7b6c9d320570855db6b
alex-odhis <lxodhiambo@gmail.com>
2022-06-28 09:57:54 +0000

    a Python script that prints The Zen of Python, by TimPeters, followed by a new line.

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
-         language that combines remarkable power with very clear syntax"
-         # YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
-         print(str)
+        language that combines remarkable power with very clear syntax"
+str = str.split()[6] + " " + str.split()[13] + " " + str.split()[1]
+print(str)
