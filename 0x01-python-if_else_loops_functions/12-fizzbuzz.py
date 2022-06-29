f902938292315f9986b2aed045506766a7b76a60
alex-odhis <lxodhiambo@gmail.com>
2022-06-29 16:59:12 +0000

    a function that prints the numbers from 1 to 100 separated by a space

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -4,8 +4,8 @@ def fizzbuzz():
     for i in range(1, 101):
         if i % 3 == 0:
             print("Fizz", end="")
-            if i % 5 == 0:
-                print("Buzz", end="")
-                if i % 3 != 0 and i % 5 != 0:
-                    print("{:d}".format(i), end="")
-                    print(" ", end="")
+        if i % 5 == 0:
+            print("Buzz", end="")
+        if i % 3 != 0 and i % 5 != 0:
+            print("{:d}".format(i), end="")
+        print(" ", end="")
