db5d4effd2592e28d2525ca3535564c69e5c7a4f
alex-odhis <lxodhiambo@gmail.com>
2022-07-18 19:24:10 +0000

    a function that raises a name exception with a message

---
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+
+def raise_exception():
+    """
+    raise a type exception
+    """
+    raise TypeError("Error Occured")
+6.
+
+#!/usr/bin/python3
+
+
+def raise_exception_msg(message=""):
+    """"
+    raises a nameexception with message
+    """
+    raise NameError(message)
