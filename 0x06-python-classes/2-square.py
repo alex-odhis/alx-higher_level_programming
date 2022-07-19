63a7097c4ea8652b4a9d1d352c3befe694d7fc5d
alex-odhis <lxodhiambo@gmail.com>
2022-07-19 20:51:52 +0000

    Size validation

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,8 +1,18 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+@author: Alex Odhiambo
+"""
 class Square:
-    __size = ""
-
+    """Class Square that has attributes. Instantiation with size
+    Attributes:
+    size (int): The size of the square
+    """
     def __init__(self, size=0):
+        """The __init__ method for Square class
+        Args:
+        size: (:obj: 'int'): A private instance size
+        """
         self.__size = size
         while True:
             try:
