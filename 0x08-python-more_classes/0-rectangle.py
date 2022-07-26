3161e2451cf1dda67097a87e3ce2a7fc585a4dc3
alex-odhis <lxodhiambo@gmail.com>
2022-07-26 19:56:27 +0000

    an empty class Rectangle that defines a rectangle:

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*i-
+
+class Rectangle:
+    """Empty class Rectangle that defines a rectangle figure
+
+    Attributes:
+        empty
+    """
+
+    pass
