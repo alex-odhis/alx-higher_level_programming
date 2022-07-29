add168141c5b91b052137f5c28286ff221ab5e37
alex-odhis <lxodhiambo@gmail.com>
2022-07-29 12:27:26 +0000

    another correction

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -18,14 +18,8 @@ def add_integer(a, b=98):
         sum of a and b
     """
 
-    if type(a) is not int:
-        if type(a) is float and a == a and abs(a) <= 1.7976931348623158e+308:
-            a = int(a)
-        else:
-            raise TypeError("a must be an integer")
-    if type(b) is not int:
-        if type(b) is float and b == b and abs(b) <= 1.7976931348623158e+308:
-            b = int(b)
-        else:
-            raise TypeError("b must be an integer")
-    return a + b
+    if isinstance(a, (int, float)) is False:
+        raise TypeError("a must be an integer")
+    if isinstance(a, (int, float)) is False:
+        raise TypeError("b must be an integer")
+    return int(a + b)
